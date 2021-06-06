659763de96143c625c2e3affc113a31b7f465be5
locust-dev <rrrr.ty30@gmail.com>
2021-06-06 19:23:42 +0300

    Initial Commit

---
+++ SlidersColorsSwiftUIHomework/SlidersColorsSwiftUIHomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlidersColorsSwiftUIHomeworkApp.swift
+//  SlidersColorsSwiftUIHomework
+//
+//  Created by Илья Тюрин on 06.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct SlidersColorsSwiftUIHomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
